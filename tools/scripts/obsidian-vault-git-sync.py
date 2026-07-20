9f110d78323d993bcc857c8540fd35bda07d342c
xdawayer <xdawayer@gmail.com>
2026-07-20 21:29:24 +0800

    vault backup: 2026-07-20 21:29:24

--- tools/scripts/obsidian-vault-git-sync.py
+++ tools/scripts/obsidian-vault-git-sync.py
@@ -40,7 +40,7 @@ COMMON_REPO_NAMES = (
 )
 
 AUTO_MERGE_JSON_PATHS = {
-    "inbox/06-tasks/tasks/.autopilot-claims.json",
+    "inbox-maboyang/06-tasks/tasks/.autopilot-claims.json",
 }
 
 SECRET_NAME_RE = re.compile(
